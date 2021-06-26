352cf4d48c701a3e1354286e86a2d25fe25c4651
otaviopetry <otaviopetry@gmail.com>
2021-06-26 11:29:44 -0300

    [01] chore: ReactJS structure

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
