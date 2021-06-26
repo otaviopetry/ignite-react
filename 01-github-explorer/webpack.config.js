f04a1dd0b26b0a3f20056bcdcba1e4201c917f69
otaviopetry <otaviopetry@gmail.com>
2021-06-26 12:25:16 -0300

    [01] chore: add sass support

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -33,11 +33,12 @@ module.exports = {
         use: 'babel-loader',
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         exclude: /node_modules/,
         use: [
           'style-loader',
           'css-loader',
+          'sass-loader',
         ],
       },
     ],
