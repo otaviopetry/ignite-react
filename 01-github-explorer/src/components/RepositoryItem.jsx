7ec8bec5c6347b8c9cc88c9389dea248906abba2
otaviopetry <otaviopetry@gmail.com>
2021-06-27 10:41:02 -0300

    [01] feat: first components
    RepositoryList and RepositoryItem

---
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,10 @@
+export function RepositoryItem(props) {
+  return (
+    <li>
+      <strong>{ props.repository?.name ?? 'default' }</strong>
+      <p>{ props.repository?.description ?? 'default' }</p>
+
+      <a href={ props.repository?.link ?? '' }>Acessar repositório</a>
+    </li>
+  );
+}
