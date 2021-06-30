61388daeebf9c1405bc8de6126b8b9666d1ab58f
otaviopetry <otaviopetry@gmail.com>
2021-06-30 09:41:16 -0300

    [01] chore: configure typescript

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -7,7 +7,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -16,6 +16,8 @@ module.exports = {
     extensions: [
       '.js', 
       '.jsx',
+      '.ts',
+      '.tsx',
     ],
   },
   devServer: {
@@ -31,7 +33,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
