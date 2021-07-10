2d9db7f0880ac9407488f6e2f9554f1e107380d9
otaviopetry <otaviopetry@gmail.com>
2021-07-10 13:50:42 -0300

    [02] feat: transactions table basic structure

--- 02-dtmoney/src/components/Dashboard/index.tsx
+++ 02-dtmoney/src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { Container } from "./styles";
 
 import { Summary } from '../Summary/index';
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
   return (
     <Container>
-      <Summary />            
+      <Summary />
+      <TransactionsTable />         
     </Container>
   );
 }
