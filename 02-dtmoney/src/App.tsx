ec97dbd8b5147e6c71a9c1f84b2c0b695c271c67
otaviopetry <otaviopetry@gmail.com>
2021-07-12 09:04:10 -0300

    [02] feat: transaction modal

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header/";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpened, setIsNewTransactionModalOpened] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpened(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpened(false);
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpened}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
